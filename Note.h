cc147ae8b6a66afe5aa75df9947bea0e991765e5
Mikko Tamminen <mikko.k.tamminen@gmail.com>
2016-02-09 10:20:57 +0200

    Added License information

--- Note.h
+++ Note.h
@@ -1,15 +1,20 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+  Copyright (C) 2016 Mikko Tamminen
 
-/* 
- * File:   Note.h
- * Author: mothi
- *
- * Created on February 7, 2016, 2:05 AM
- */
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 
 #ifndef NOTE_H
 #define NOTE_H
